8b3f3241949dc1d7591f750bf3a87884c261e696
Shaik Jakeer Hussain <hussainjakeer760@gmail.com>
2024-11-15 20:42:04 +0530

    Initial commit

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import os
 from app import create_app
 
-def main():
+# def main():
+#    app = create_app()
+#    host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
+#    port = int(os.getenv('FLASK_RUN_PORT', 5000))
+#    # app.run(host=host, port=port, debug=True,ssl_context = "adhoc")
+#    app.run(host=host, port=port, debug=True)
+
+if __name__ == '__main__':
+   # main()
    app = create_app()
    host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
    port = int(os.getenv('FLASK_RUN_PORT', 5000))
    # app.run(host=host, port=port, debug=True,ssl_context = "adhoc")
    app.run(host=host, port=port, debug=True)
-
-if __name__ == '__main__':
-   main()
